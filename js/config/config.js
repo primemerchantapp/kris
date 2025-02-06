4de5f0b8283a31653214fca298d7c2a91ec59cda
masterdee121212 <primemerchantapp@gmail.com>
2025-02-07 03:42:13 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API: {
-    KEY: "AIzaSyDj5JnCRB80xw2H-JrWt1x1fz9vDbxE46U",
+    KEY: "AIzaSyDHGlDAX5wzGJj6x77hZvTvWggGCgR97Ok",
     BASE_URL: "wss://generativelanguage.googleapis.com/ws",
     VERSION: "v1alpha",
     MODEL_NAME: "models/gemini-2.0-flash-exp",
